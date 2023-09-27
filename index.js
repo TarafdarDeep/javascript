e4afe16c965abc650bddc3da309a3002837a3bae
TarafdarDeep <deeptarafdar9564558232@gmail.com>
2023-09-27 22:21:52 +0530

    1st js

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    console.log(a+b);
+}
+add(10,22)
+const add2 = function(a,b){
+    console.log(a+b)
+}
+add2(150,22)
+console.log(add);
+
+var result = add(5,4);
+console.log(result);
+
+var result2=add2(10,25);
+console.log(result2);
